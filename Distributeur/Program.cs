b9ee0b5ad33d9ffd60f2d376706ae2e0bf621b58
Bargaoui Imed <bargaoui.imed@hotmail.com>
2021-09-14 12:32:26 +0200

    Ajoutez des fichiers projet.

---
+++ Distributeur/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distributeur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selectionner botre boisson");
+            Console.WriteLine("Expresso  => [1]");
+            Console.WriteLine("Allongé   => [2]");
+            Console.WriteLine("Capuccino => [3]");
+            Console.WriteLine("Chocolat  => [4]");
+            Console.WriteLine("The       => [5]");
+            try
+            {
+            int num = int.Parse(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine(new Expresso());
+                    break;
+                case 2:
+                    Console.WriteLine(new Allenge());
+                    break;
+                case 3:
+                    Console.WriteLine(new Capuccino());
+                    break;
+                case 4:
+                    Console.WriteLine(new Chocolat());
+                    break;
+                case 5:
+                    Console.WriteLine(new The());
+                    break;
+                default:
+                    Console.WriteLine("Votre choix n'existe pas..");
+                    break;
+            }
+
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+               
+            }finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Distributeur/Recette.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distributeur
+{
+    public abstract class Recette
+    {
+        public double Eau = 0.2;
+        public double marge = 30;
+
+        public abstract double GetValue();
+
+        public double GetPrice()
+        {
+            return GetValue() + (GetValue() * marge / 100);
+        }
+
+        public override string ToString()
+        {
+            return "Le prix est : " + GetPrice() + " Eur";
+        }
+    }
+
+
+    public class Expresso : Recette
+    {
+        public double Cafe = 1;
+
+        public override double GetValue()
+        {
+            return Cafe + Eau;
+        }
+
+
+    }
+
+    public class Allenge : Expresso
+    {
+        public override double GetValue()
+        {
+            return 2 * Cafe + Eau;
+        }
+
+
+    }
+
+    public class Capuccino : Expresso
+    {
+        protected double Creme = 0.5;
+        protected double Choclate = 1;
+        public override double GetValue()
+        {
+            return Choclate + Creme + Cafe + Eau;
+        }
+
+
+    }
+
+    public class Chocolat : Recette
+    {
+        protected double Lait = 0.4;
+        protected double Choclate = 1;
+        protected double Sucre = 0.1;
+        public override double GetValue()
+        {
+            return 3 * Choclate + 2 * Lait + Sucre + Eau;
+        }
+    }
+
+    public class The : Recette
+    {
+        protected double Tea = 2;
+        public override double GetValue()
+        {
+            return Tea + 2 * Eau;
+        }
+
+    }
+
+}
